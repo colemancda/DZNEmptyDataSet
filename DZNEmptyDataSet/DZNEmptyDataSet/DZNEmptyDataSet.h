636b5244d4d1b47c5250177ede3b6d5a9ed02c8a
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-06-08 15:30:48 -0500

    created framework

---
+++ DZNEmptyDataSet/DZNEmptyDataSet/DZNEmptyDataSet.h
@@ -0,0 +1,18 @@
+//
+//  DZNEmptyDataSet.h
+//  DZNEmptyDataSet
+//
+//  Created by Alsey Coleman Miller on 6/8/15.
+//  Copyright (c) 2015 dzenbot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DZNEmptyDataSet.
+FOUNDATION_EXPORT double DZNEmptyDataSetVersionNumber;
+
+//! Project version string for DZNEmptyDataSet.
+FOUNDATION_EXPORT const unsigned char DZNEmptyDataSetVersionString[];
+
+#import <DZNEmptyDataSet/UIScrollView+EmptyDataSet.h>
+
